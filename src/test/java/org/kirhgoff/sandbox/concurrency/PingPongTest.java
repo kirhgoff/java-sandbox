24bf7e0964f3ddb6e4b74bdeb24c6a5ad421a1b5
Kirill Lastovirya <kirill.lastovirya@gmail.com>
2015-10-20 01:47:31 +0300

    trying to fix the order

--- src/test/java/org/kirhgoff/sandbox/concurrency/PingPongTest.java
+++ src/test/java/org/kirhgoff/sandbox/concurrency/PingPongTest.java
@@ -2,9 +2,6 @@ package org.kirhgoff.sandbox.concurrency;
 
 import org.testng.annotations.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 /**
@@ -13,53 +10,51 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class PingPongTest {
   @Test
   public void testPingPong() throws Exception {
-    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    PrintStream out = new PrintStream(baos);
-    runPingPong(out);
+    StringBuilder sb = new StringBuilder();
+    runPingPong(sb);
     Thread.sleep(500);
-    out.flush();
-    assertThat (baos.toString("UTF-8")).isEqualTo("Ping\nPong\nPing\nPong\nPing\nPong\n");
+    assertThat(sb.toString()).isEqualTo("Ping\nPong\nPing\nPong\nPing\nPong\n");
   }
 
-  private void runPingPong(PrintStream out) {
-    new Thread(new PingPong(out, "Ping")).start();
-    new Thread(new PingPong(out, "Pong")).start();
+  private void runPingPong(StringBuilder out) throws InterruptedException {
+    new Thread(new PingPong(out, "Pong\n")).start();
+    new Thread(new PingPong(out, "Ping\n")).start();
   }
 
   private class PingPong implements Runnable {
-    private final PrintStream out;
+    private final StringBuilder sb;
     private final String text;
     private int counter = 0;
 
-    public PingPong(PrintStream out, String text) {
-      this.out = out;
+    public PingPong(StringBuilder sb, String text) {
+      this.sb = sb;
       this.text = text;
     }
 
     @Override
     public void run() {
-      if (text.equals("Pong")) {
-        synchronized (out) {
-          try {
-            out.wait();
-          } catch (InterruptedException e) {
-            e.printStackTrace();
-          }
-        }
-      }
       while (counter < 3) {
-        synchronized (out) {
-          try {
-
-            System.out.println(text);
-            out.println(text);
-            counter ++;
-            out.notify();
-            out.wait();
-          } catch (InterruptedException e) {
-            e.printStackTrace();
+        try {
+          synchronized (sb) {
+            if (text.startsWith("Pong")) {
+              while (!sb.toString().endsWith(text)) {
+                System.out.print("Waiting for not " + text);
+                sb.wait();
+              }
+            }
           }
+
+          synchronized (sb) {
+            System.out.print("Appending " + text);
+            sb.append(text);
+            counter++;
+            sb.notifyAll();
+            sb.wait();
+          }
+        } catch (InterruptedException e) {
+          e.printStackTrace();
         }
+
       }
     }
   }
