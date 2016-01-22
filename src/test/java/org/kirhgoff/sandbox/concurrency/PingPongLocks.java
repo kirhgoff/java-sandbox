8739dbfbf533dc133653133017a65c0dcb3120d5
Kirill Lastovirya <kirill.lastovirya@gmail.com>
2016-01-23 00:15:41 +0300

    implemented ping pong on locks

--- src/test/java/org/kirhgoff/sandbox/concurrency/PingPongLocks.java
+++ src/test/java/org/kirhgoff/sandbox/concurrency/PingPongLocks.java
@@ -1,5 +1,6 @@
 package org.kirhgoff.sandbox.concurrency;
 
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -14,33 +15,73 @@ public class PingPongLocks implements Runnable{
     PING = ping;
     PONG = pong;
     Lock lock = new ReentrantLock();
+    Condition pingCondition = lock.newCondition();
+    Condition pongCondition = lock.newCondition();
 
-    return new Runnable [] {new PingPongLocks(sb, ping, lock), new PingPongLocks(sb, pong, lock)};
+    return new Runnable [] {
+        new PingPongLocks(sb, ping, lock, pingCondition, pongCondition),
+        new PingPongLocks(sb, pong, lock, pongCondition, pingCondition)
+    };
   }
 
   private final StringBuilder sb;
   private final String text;
-  private Lock lock;
+  private final Lock lock;
+  private final Condition thisCondition;
+  private final Condition otherCondition;
   private int counter = 0;
 
-  public PingPongLocks(StringBuilder sb, String text, Lock lock) {
+  public PingPongLocks(
+      StringBuilder sb,
+      String text,
+      Lock lock,
+      Condition thisCondition,
+      Condition otherCondition
+  )
+  {
     this.sb = sb;
     this.text = text;
     this.lock = lock;
+    this.thisCondition = thisCondition;
+    this.otherCondition = otherCondition;
   }
 
   @Override
   public void run() {
-    while (counter < 3) {
-      lock.lock();
-      try {
-        System.out.print("Appending " + text);
-        sb.append(text);
-        counter++;
-      } finally {
-        lock.unlock();
+    try {
+      while (counter < 3) {
+        if (text.startsWith(PONG)) {
+          while (sb.toString().isEmpty()) {
+            System.out.print("Waiting for not " + text);
+            otherCondition.wait();
+          }
+        }
+
+        lock.lock();
+        try {
+          System.out.print("Appending " + text);
+          sb.append(text);
+          counter++;
+          otherCondition.signal();
+          thisCondition.await();
+        } finally {
+          lock.unlock();
+        }
       }
+
+      if (text.startsWith(PING)) {
+        System.out.print("Notify from " + text);
+        lock.lock();
+        try {
+          otherCondition.signal();
+        } finally {
+          lock.unlock();
+        }
+      }
+
+      System.out.print("Exiting " + text);
+    } catch(InterruptedException e) {
+      e.printStackTrace();
     }
-    System.out.print("Exiting " + text);
   }
 }
